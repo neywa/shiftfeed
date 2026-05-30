ce76b00967f8ac972f30ed1efce8af7911c8841b
neywa <neywa@users.noreply.github.com>
2026-05-30 08:45:26 +0000

    deploy: d588ec1174eb41024937f11a02a8ece38add94bb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1813943526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "135202022" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
